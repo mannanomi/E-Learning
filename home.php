a920db2122f5468b5fa704a2c3dd3f25d1a1ce08
Mannan A. Omi <mannanomi@gmail.com>
2020-05-07 21:14:50 +0600

    Student home and index page :tada:

---
+++ home.php
@@ -0,0 +1,59 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Vision Statement of CSE Department</h4>
+    <p>
+To be internationally renowned in Computer Science and Engineering and to exalt excellence in education, research and industrial profession for sustainable transformation of the society.
+</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Mission of CSE Department</h4> 
+  <p>Center of Academic Excellence Delivering Quality Service to all.</p>
+  <h4>Mission</h4>
+  <p> To advance knowledge and learning of evolving challenges in Computer Science and Engineering through quality education and research towards the development of the society.</p>
+  <h4>Goals</h4> 
+  <ol>
+    <li>Globally competitive graduates</li>  
+    <li>Institutionalize research culture</li> 
+    <li>Responsive and sustainable extension services</li> 
+    <li>Maximized profit of variable agro-industrial business ventures</li>  
+    <li>Effective and efficient administration </li>  
+  </ol>
+
+  <h4>Objectives</h4> 
+   <ol>
+      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
+      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
+      <li>Produce highly competitive graduates.</li>
+      <li>Conduct relevant and updated researches</li>
+      <li>Extend financial support and manpower for outreach activities.</li> 
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li> Academic Standards</li>  
+    </ul>
+<h4>Core Values</h4>
+  <ul>
+    <li>God-Centered</li>
+    <li>Excellence</li>
+    <li>Integrity</li>
+    <li>Transparency and Accountability</li>
+    <li>Dedication to Quality Service</li>  
+  </ul> 
+<h4>Quality Policy</h4> 
+<ol>
+    <li>EWU is committed to be the center of academic excellence delivering quality service to all by;</li> 
+    <li>Continuing innovations and quality improvement cultivating an efficient and effective and environment for maximum clientele satisfaction;</li> 
+    <li>Adhering to laws and regulations, global standards, and environmental change requirements;</li> 
+    <li>Participating in sustainable developments projects for inclusive economic for inclusive 
+economic growth;</li> 
+    <li>Showcasing quality outputs; and,</li> 
+    <li>Upholding values and integrity and nurturing talents and skills for global competitiveness.</li>  
+</ol>
+</div>
+ 
+
